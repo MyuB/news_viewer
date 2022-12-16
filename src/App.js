987894b11d20723da1d172097a98f0930f10b015
MyuB <mjycoco0@naver.com>
2022-12-16 20:53:11 +0900

    news header created

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import { useState } from 'react';
 import axios from 'axios';
 import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
 const App = () => {
-  // const [data, setData] = useState(null);
-
-  // const url = 'https://jsonplaceholder.typicode.com/todos/1';
-  // const onClick = () => {
-  //   axios.get(url).then((response) => {
-  //     setData(response.data);
-  //   });
-  // };
-  // return (
-  //   <div>
-  //     <div>
-  //       <button onClick={onClick}>불러오기</button>
-  //     </div>
-  //     {data && (
-  //       <textarea
-  //         rows={7}
-  //         value={JSON.stringify(data, null, 2)}
-  //         readOnly={true}
-  //       />
-  //     )}
-  //   </div>
-  // );
-  return <NewsList />;
+  return (
+    <>
+      <Categories />
+      <NewsList />
+    </>
+  );
 };
 
 export default App;
